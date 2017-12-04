c0fbe68e34dc89d5f1738b3a2a30bac2cc1ff167
Yunhe (John) Wang <yhjw88@gmail.com>
2017-12-04 03:39:04 -0800

    New code for sentInUcs

--- sentInGenerator.py
+++ sentInGenerator.py
@@ -12,6 +12,7 @@ def getSections(collection,
                 minSentencesPerSection,
                 numSectionsToUse):
     sections = []
+    articles = []
     numToSkip = random.randint(articlesToSkipRange[0], articlesToSkipRange[1])
     for articleDict in collection.find():
         # Skip this article if required.
@@ -28,7 +29,7 @@ def getSections(collection,
             article = article[:seeAlsoStartIndex]
 
         # Convert into sections and sentences.
-        sectionsPerArticle = random.randint(sectionsPerArticleRange[0], sectionsPerArticleRange[1])
+        articleSentences = []
         articleSections = []
         tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
         for section in re.split("==[^\n]*==", article):
@@ -38,23 +39,28 @@ def getSections(collection,
                 in tokenizer.tokenize(section)
                 if sentence.strip()
             ]
-            if len(sentences) < minSentencesPerSection:
-                continue
+            articleSentences.extend(sentences)
+            if len(sentences) >= minSentencesPerSection:
+                articleSections.append(sentences)
 
-            articleSections.append(sentences)
-            if len(articleSections) >= sectionsPerArticle:
-                sections.extend(articleSections)
-                if len(sections) % 100 < len(articleSections):
-                    print "Got {} sections.".format(len(sections))
-                    sys.stdout.flush()
-                break
+        # Extract sections to use.
+        if len(articleSections) == 0:
+            continue
+        sectionsPerArticle = random.randint(sectionsPerArticleRange[0], sectionsPerArticleRange[1])
+        sectionsToUseThisArticle = random.sample(articleSections, min(sectionsPerArticle, len(articleSections)))
+        sections.extend(sectionsToUseThisArticle)
+        articles.extend(len(sectionsToUseThisArticle) * [articleSentences])
+        if len(sections) % 100 < len(sectionsToUseThisArticle):
+            print "Got {} sections.".format(len(sections))
+            sys.stdout.flush()
 
         # Check if we're done.
         if len(sections) >= numSectionsToUse:
             sections = sections[:numSectionsToUse]
+            articles = articles[:numSectionsToUse]
             break
 
-    return sections
+    return sections, articles
 
 # Finds where the given sentences are inserted into the given section.
 def getInsertionIndexes(sentences, section):
@@ -71,51 +77,49 @@ def dumpToFile(dataList, filename):
     with open(filename, 'wb') as outFile:
         print "Dumping {} to {}".format(len(dataList), filename)
         sys.stdout.flush()
-        outFile.write(json.dumps(dataList))
+        outFile.write(json.dumps(dataList, indent=4))
 
 def main():
     # Data Format
-    # { prelimSection: [...] section: [...], sentences: [...], insertionIndexes: [...] }
+    # { prelimSection: [...] article: [...] section: [...], sentences: [...], insertionIndexes: [...] }
     # One insertionIndex per sentence in sentences.
     # insertionIndex is n, 0 <= n <= len(section), or None.
     # 0 section[0] 1 section[1] 2 section[2] 3 section[3] 4
     # Small assumption: If insertionIndex is the same, randomly pick one.
     # All ranges are inclusive.
-    numSectionsToUseThousands = 5
+    numSectionsTrain = 24000
+    numSectionsDev = 8000
+    numSectionsTest = 8000
     articlesToSkipRange = [0, 10]
-    sectionsPerArticleRange = [1, 3]
-    minSentencesPerSection = 12
-    numCorrectSentencesRange = [1, 2]
+    sectionsPerArticleRange = [1, 1]
+    minSentencesPerSection = 10
+    numCorrectSentencesRange = [1, 1]
     # TODO: numIncorrectSentencesRange = [0, 3]
     outFilePrefix = "data/sentIn"
 
     client = MongoClient()
     collection = client.cs229.wArticlesCleaned
 
-    # Split data.
-    numSectionsToUse = int(numSectionsToUseThousands) * 1000
-    numSectionsTest = int(numSectionsToUseThousands * 0.2) * 1000
-    numSectionsDev = numSectionsTest
-    numSectionsTrain = numSectionsToUse - numSectionsTest - numSectionsDev
     print "Train: {} Dev: {} Test: {}".format(numSectionsTrain, numSectionsDev, numSectionsTest)
     sys.stdout.flush()
 
     # First get all our required sections.
-    prelimSections = getSections(collection,
-                                 articlesToSkipRange,
-                                 sectionsPerArticleRange,
-                                 minSentencesPerSection,
-                                 numSectionsToUse)
+    prelimSections, articles = getSections(collection,
+                                           articlesToSkipRange,
+                                           sectionsPerArticleRange,
+                                           minSentencesPerSection,
+                                           numSectionsTrain + numSectionsDev + numSectionsTest)
 
     # Go through the sections and extract sentences.
     dataList = []
-    for prelimSection in prelimSections:
+    for prelimSection, article in zip(prelimSections, articles):
         data = {}
         numCorrectSentences = random.randint(numCorrectSentencesRange[0], numCorrectSentencesRange[1])
         data["prelimSection"] = prelimSection
+        data["article"] = article
         data["sentences"] = random.sample(prelimSection, numCorrectSentences)
+        random.shuffle(data["sentences"])
         data["section"] = [sentence for sentence in prelimSection if (sentence not in data["sentences"])]
-        random.shuffle(data["section"])
         data["insertionIndexes"] = getInsertionIndexes(data["sentences"],  prelimSection)
         dataList.append(data)
 
